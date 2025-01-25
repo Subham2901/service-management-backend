8f93af9b3ca8bc189c430026119e0873b9e1f05c
Subham Singh <subham.reek@gmail.com>
2025-01-25 14:22:48 +0100

    Update service-requests.service.ts

--- src/service-requests/service-requests.service.ts
+++ src/service-requests/service-requests.service.ts
@@ -199,7 +199,7 @@ async getServiceRequestDetails(id: string): Promise<ServiceRequest> {
         selectedMembers: mappedMembers,
         numberOfSpecialists: totalSpecialists,
         createdBy,
-        cycleStatus: 'cycle1',
+        cycleStatus: 'Cycle1',
         status: 'submitted',
       });
 
